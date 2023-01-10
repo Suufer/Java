8e7932c1d6f881f2e89f0e7cb8a7545894e5cbf9
Suufer <122040357+Suufer@users.noreply.github.com>
2023-01-10 19:56:56 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int percent;
+        int sum;
+        if (amount < 100) {
+            percent = 0;
+        } else {
+            percent = 1;
+        }
+        System.out.println(amount);
+    }
+
+}
