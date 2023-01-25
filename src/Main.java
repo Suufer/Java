e320bdcee1ed58642f76068a9c48dd13696c6dab
Suufer <122040357+Suufer@users.noreply.github.com>
2023-01-25 16:35:33 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice;
+        ticketPrice = 20_000;
+        int bonusMiles = ticketPrice / 20 / 100;
+        System.out.println(bonusMiles);
+    }
+}
+
+
